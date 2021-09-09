49d4d0e4a8e7b1a50bc6a30bcd27fd0c923ea924
magus <martingramajo08@gmail.com>
2021-09-09 16:08:10 -0300

    agrego responsive

--- src/Components/Publicidad.jsx
+++ src/Components/Publicidad.jsx
@@ -9,7 +9,7 @@ export default function Publicidad({ data }) {
             <div className="container my-5">
                 <div className="row">
                     <Image className="forma col-lg-12 col-sm-6 mt-4 mb-5 m-auto"  src={image}  roundedCircle/>
-                    <div className="col-lg-6 col-sm-12 m-auto">
+                    <div className="col-lg-6 col-sm-12 m-auto text-center">
                         <h2>{titulo}</h2>
                         <p className="mt-4"> {descripcion}</p>
                         <Button className="col-lg-12 col-sm-6 m-auto "  variant="warning" size="lg">Learn More</Button>{' '}
