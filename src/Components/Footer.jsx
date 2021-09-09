27386cfb3c6f62a478490fb7e4bb708bc2a035a5
magus <martingramajo08@gmail.com>
2021-09-09 16:09:00 -0300

    modifico el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,44 +1,31 @@
+import { Nav, Navbar } from "react-bootstrap";
 import "./footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faTwitterSquare, faLinkedin, faGooglePlusSquare  } from "@fortawesome/free-brands-svg-icons";
-
+import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faMotorcycle } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
-    <div className="main-footer mt-5">
+    <div className="main-footer mt-2">
       <div className="container">
-        <div className="row ">
-          {/* Column1 */}
-          <div className="col d-flex  justify-content-center ">
-            <h1 className="list-unstyled">
-                          <li> <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faFacebook} /> </a>  </li>
-                          <li> <a href="https://twitter.com/home" target="_blank" rel="noreferrer"> <FontAwesomeIcon icon={faTwitterSquare} /> </a> </li>
-                          <li> <a href="https://ar.linkedin.com/" target="_blank" rel="noreferrer"> <FontAwesomeIcon icon={faLinkedin} /> </a></li>
-                          <li> <a href="https://myaccount.google.com/profile" target="_blank" rel="noreferrer"> <FontAwesomeIcon icon={faGooglePlusSquare} /> </a> </li>
-            </h1>
+        <div className="row text-white d-flex justify-content-evenly">
+          <Nav className=" justify-content-center">
+            <Navbar.Brand href="#"> <FontAwesomeIcon icon={faMotorcycle} /> Scoot</Navbar.Brand>
+            <Nav.Link href="#action1">About</Nav.Link>
+            <Nav.Link href="#action2">Location</Nav.Link>
+            <Nav.Link href="#action2">Carrers</Nav.Link>
+            <div className=" d-flex m-auto mx-5 mb-1 fs-1 ">
+              <Nav.Link href="#action1"><FontAwesomeIcon icon={faFacebook} /></Nav.Link>
+              <Nav.Link href="#action1"><FontAwesomeIcon icon={faTwitter} /></Nav.Link>
+              <Nav.Link href="#action1"><FontAwesomeIcon icon={faInstagram} /></Nav.Link>
           </div>
-          {/* Column2 */}
-          <div className="col d-flex  justify-content-center">
-            <ul className="list-unstyled">
-              <li>Contact Us (Central Office)</li>
-              <li>P : +1 253-863-8967</li>
-              <li>M :Scoot@scoot.com</li>
-            </ul>
-          </div>
-          {/* Column3 */}
-          <div className="col d-flex  justify-content-center">
-            <ul className="list-unstyled">
-              <li>Scoot Central Office</li>
-              <li>3886 Wellington Street</li>
-              <li>Toronto, Ontario M9C 3J5</li>
-            </ul>
-          </div>
+          </Nav>
+          <hr />
         </div>
-        <hr />
         <div className="row">
           <p className="col-sm-12 m-auto d-flex justify-content-center">
-            &copy;{new Date().getFullYear()} | All rights reserved |
-            Terms Of Service | Privacy
+            &copy;{new Date().getFullYear()} | All rights reserved | Terms Of
+            Service | Privacy
           </p>
         </div>
       </div>
