3b6b06e8b934bf15401d03c643419218dbfc7e86
magus <martingramajo08@gmail.com>
2021-09-09 16:09:17 -0300

    agrego componente Banner con su css

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import "./banner.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faApple, faGooglePlay } from "@fortawesome/free-brands-svg-icons";
+
+export default function Banner() {
+  return (
+    <div className=" container banner">
+      <div className="row">
+        <div className="col-md-6 m-auto">
+          <h1 className="text-white fs-1">Sing up and scoot off today</h1>
+        </div>
+        <div className="col-md-6 m-auto p-1 mb-2">
+          <Button className="my-5 mx-5 p-2" variant="outline-dark">
+            <span className="text-white">Available on the AppStore </span>
+            <div>
+              <FontAwesomeIcon className="fs-1 mx-1" icon={faApple} />
+            </div>
+          </Button>
+          <Button className="my-5 mx-5 p-2" variant="outline-dark">
+          <span className="text-white">Get it on GooglePlay </span>
+            <div>
+              <FontAwesomeIcon className="fs-1 mx-1" icon={faGooglePlay} />
+            </div>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
